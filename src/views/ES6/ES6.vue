edfdee92670430bff70a2bd4aef7e91d3e001aa6
liu <liuyi960422@163.com>
2020-06-08 16:30:44 +0800

    es6

--- src/views/ES6/ES6.vue
+++ src/views/ES6/ES6.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="es6">es6</div>
+  <div class="es6">
+    <variable></variable>
+    <regexp></regexp>
+    <string></string>
+    <number></number>
+  </div>
 </template>
 
 <script>
+const variable = () => import('./childCpns/01Variable')
+const regexp = () => import('./childCpns/02RegExp')
+const string = () => import('./childCpns/03String')
+const number = () => import('./childCpns/04Number')
 export default {
-  name: 'ES6'
+  name: 'ES6',
+  components: {
+    variable,
+    regexp,
+    string,
+    number
+  }
 }
 </script>
 
